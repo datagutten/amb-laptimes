efea58fde45564642a4bbf8fe33f5a33ffcc276d
datagutten <datagutten@datagutten.net>
2020-05-01 14:33:46 +0200

    New config and error handling

--- passing_saver.php
+++ passing_saver.php
@@ -1,6 +1,7 @@
 <?Php
 
 use datagutten\amb\laps\passing_db;
+use datagutten\amb\parser;
 use datagutten\amb\parser\socket;
 
 require 'vendor/autoload.php';
@@ -8,34 +9,84 @@ $parser=new amb_p3_parser;
 
 
 //Capture data from a AMB decoder using a socket
-if(isset($argv[1]) && file_exists($conf=dirname(__FILE__)."/config_socket_{$argv[1]}.php"))
-	require $conf;
-else
-	die("Missing config name\n");
 
-$socket = new socket($address, $port);
+$decoders = @include 'config_decoders.php';
+if(empty($decoders))
+    die("Missing decoder config file\n");
+
+if(!isset($argv[1]))
+    die("Usage: passing_saver.php [decoder name]");
+if(!isset($decoders[$argv[1]]))
+    die("Invalid decoder\n");
+else
+    $decoder = $decoders[$argv[1]];
+
+$debug = isset($argv[2]);
+
+if(!isset($decoder['address']))
+    die("Decoder address not set in config file\n");
+if(!isset($decoder['port']))
+    $decoder['port'] = 5403;
+
+try {
+    $socket = new socket($decoder['address'], $decoder['port']);
+    $db = new passing_db($decoder['id']);
+    $db->init();
+}
+catch (parser\exceptions\ConnectionError $e)
+{
+    die($e->getMessage()."\n");
+}
+catch (FileNotFoundException $e)
+{
+    die("Missing database configuration file\n");
+}
 
 while (true) 
 {
-    $records = $socket->read_records();
+    try {
+        $records = $socket->read_records();
+    }
+    catch (parser\exceptions\ConnectionError $e)
+    {
+        die($e->getMessage()."\n");
+    }
+
     if(empty($records))
         continue;
+    if(!isset($decoder_validated))
+    {
+        try {
+            $record_parsed = $parser->parse($records[0]);
+            if (dechex($record_parsed['DECODER_ID']) != $decoder['id'])
+                die(sprintf("Received message from decoder %s, expected %s\n", $record_parsed['DECODER_ID'], $decoder['id']));
+            else
+                $decoder_validated = true;
+        }
+        catch (parser\exceptions\AmbParseError $e)
+        {
+            continue;
+        }
+    }
 
 	foreach($records as $record)
 	{
-		$record_parsed=$parser->parse($record,0x01);
+	    try {
+            $record_parsed = $parser->parse($record, 0x01);
+        }
+        catch (parser\exceptions\AmbParseError $e)
+        {
+            echo $e->getMessage()."\n";
+            continue;
+        }
 		if($record_parsed===true)
         {
-            //echo "Skip invalid type\n";
+            if($debug)
+                echo "Skip invalid type\n";
             continue;
         }
 
 		print_r($record_parsed);
-		if(!isset($db))
-		{
-			$db=new passing_db(dechex($record_parsed['DECODER_ID']));
-			$db->init();
-		}
 		if(!is_array($record_parsed))
 			continue;
 		$db->insert($record_parsed);
