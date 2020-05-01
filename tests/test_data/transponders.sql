45aedd70e7a8af5a1e01c985f56e03398ce56f04
datagutten <datagutten@datagutten.net>
2020-05-01 14:14:39 +0200

    Create transponder table

---
+++ tests/test_data/transponders.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `transponders` (
+  `transponder_id` int(11) NOT NULL,
+  `transponder` varchar(45) DEFAULT NULL COMMENT 'Transponder name',
+  `nickname` varchar(255) DEFAULT NULL COMMENT 'User nick name',
+  `avatar` text DEFAULT NULL COMMENT 'Base64 encoded profile picture',
+  PRIMARY KEY (`transponder_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
