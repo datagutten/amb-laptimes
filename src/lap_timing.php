fe9a49cc585e5ff23586d9e3d3c65b77fa917972
datagutten <datagutten@datagutten.net>
2021-01-16 10:24:18 +0100

    Replace laps() with rounds() and add other new methods

--- src/lap_timing.php
+++ src/lap_timing.php
@@ -4,6 +4,11 @@
 namespace datagutten\amb\laps;
 
 
+use InvalidArgumentException;
+use PDO;
+use PDOException;
+use UnexpectedValueException;
+
 class lap_timing extends passing_db
 {
 	public $time_offset_hours=0;
@@ -27,8 +32,98 @@ class lap_timing extends passing_db
 	}
 
     /**
+     * @param array $passing1 First passing
+     * @param array $passing2 Last passing
+     * @param int $limit
+     * @return int Round time in seconds
+     */
+    public static function lap_time($passing1, $passing2, $limit = 60)
+    {
+        $round_time=$passing2['rtc_time']-$passing1['rtc_time']; //Calculate lap time
+        $round_time_seconds=$round_time/pow(1000,2); //Convert lap time to seconds
+        if($round_time_seconds>$limit) {
+            //printf("time %d is above limit %d\n", $round_time_seconds, $limit);
+            return null;
+        }
+        return $round_time_seconds;
+    }
+
+    /**
+     * Convert decoder time to valid unix timestamp
+     * @param int $time
+     * @return int
+     */
+    public static function convert_time($time)
+    {
+        return intval($time/pow(1000,2));
+    }
+
+    /**
+     * Get last rounds
+     * @param int $lap_count_limit Limit the number of laps returned
+     * @param int $lap_time_limit Lap times above this limit will be ignored
+     * @param int $time_before Only fetch passings before time
+     * @return array
+     * @throws PDOException Database error
+     */
+    function laps($lap_count_limit = 90, $lap_time_limit = 60, $time_before = 0)
+    {
+        if (!is_numeric($lap_count_limit))
+            throw new UnexpectedValueException('Lap count limit is not numeric');
+        if (!is_numeric($lap_time_limit))
+            throw new UnexpectedValueException('Lap time limit is not numeric');
+        if (!is_numeric($time_before))
+            throw new UnexpectedValueException('Time before is not numeric');
+
+        //The returned number of passings might be lower than the limit because the limit includes invalid passings
+        //TODO: Count and limit rounds instead of passings
+        if($time_before == 0)
+            $passings = $this->db->query(sprintf('SELECT * FROM %s ORDER BY rtc_time DESC LIMIT %d', $this->table, $lap_count_limit*2));
+        else
+            $passings = $this->db->query(sprintf('SELECT * FROM %s WHERE rtc_time<%d ORDER BY rtc_time DESC LIMIT %d', $this->table, $time_before, $lap_count_limit*2));
+        $car_prev_passing = [];
+        $count = 0;
+        $laps = [];
+        foreach ($passings as $passing)
+        {
+            if(isset($car_prev_passing[$passing['transponder']]))
+            {
+                $next_passing = $car_prev_passing[$passing['transponder']];
+                $lap_time = self::lap_time($passing, $next_passing, $lap_time_limit);
+                if(empty($lap_time)) //Invalid lap time
+                {
+                    continue;
+                }
+
+                $laps[] = [
+                    'lap_time'=>$lap_time,
+                    'transponder'=>(int)$passing['transponder'],
+                    'start_time'=>self::convert_time($passing['rtc_time']),
+                    'end_time'=>self::convert_time($next_passing['rtc_time']),
+                    'start_number'=>(int)$passing['passing_number'],
+                    'end_number'=>(int)$next_passing['passing_number'],
+                ];
+                $count++;
+            }
+
+            $car_prev_passing[$passing['transponder']] = $passing;
+            if($count>=$lap_count_limit)
+                break;
+        }
+        if($count<$lap_count_limit && !empty($passing))
+        {
+            //printf("Have %d/%d, want %d, missing %d, got %d passings\n", $count, count($laps), $lap_count_limit, $lap_count_limit-$count, $passings->rowCount());
+            $laps2 = $this->laps($lap_count_limit-$count, $lap_time_limit, $passing['rtc_time']);
+            //printf("Got %d more\n", count($laps2));
+
+            $laps = array_merge($laps, $laps2);
+        }
+        return $laps;
+    }
+
+    /**
      * Calculate stats for rounds
-     * @param array $rounds Return value from rounds()
+     * @param array $rounds Return value from laps()
      * @param bool $save_debug_info Save files with debug information
      * @return array Argument array with added values for CSS class and best time
      */
@@ -38,26 +133,27 @@ class lap_timing extends passing_db
 		//First round has highest key
 		foreach($reverse_rounds as $key=>$round) //First round first
 		{
+		    //$round['lap_time'] = $round['lapTime'];
 			$transponder=$round['transponder'];
 			if(!isset($rounds[$key+1]))
 			{
-				$this->previous_round[$round['transponder']]=$round['lapTime'];
+				$this->previous_round[$round['transponder']]=$round['lap_time'];
 				continue;
 			}
 			$debug='';
 			$debug2='';
 			if(!empty($this->previous_round[$round['transponder']]))
 			{
-				if($round['lapTime']<$this->previous_round[$round['transponder']])
+				if($round['lap_time']<$this->previous_round[$round['transponder']])
 				//if($rounds[$key]['lapTime']<$rounds[$key+1]['lapTime'])
 				{
 					$rounds[$key]['class']='faster';
-					$debug.=sprintf("Transponder %s: %s is faster than %s\n",$round['transponder'],$round['lapTime'],$this->previous_round[$round['transponder']]);
+					$debug.=sprintf("Transponder %s: %s is faster than %s\n",$round['transponder'],$round['lap_time'],$this->previous_round[$round['transponder']]);
 				}
 				else
 				{
 					$rounds[$key]['class']='slower';
-					$debug.=sprintf("Transponder %s: %s is slower than %s\n",$round['transponder'],$round['lapTime'],$this->previous_round[$round['transponder']]);
+					$debug.=sprintf("Transponder %s: %s is slower than %s\n",$round['transponder'],$round['lap_time'],$this->previous_round[$round['transponder']]);
 				}
 			}
 			if(date('Y-m-d',$round['start_time'])!=date('Y-m-d',$rounds[$key+1]['start_time']))
@@ -66,21 +162,21 @@ class lap_timing extends passing_db
 			//$best_round=$this->best_round($round['transponder'],$round['lapTime']);
 			//Find best round
 			if(empty($this->best_rounds[$transponder]))
-				$this->best_rounds[$transponder]=$round['lapTime'];
-			elseif($round['lapTime']<$this->best_rounds[$transponder])
+				$this->best_rounds[$transponder]=$round['lap_time'];
+			elseif($round['lap_time']<$this->best_rounds[$transponder])
 			{
 				$rounds[$key]['class']='best-time';
-				$rounds[$key]['best-time']=$round['lapTime'];
-				$debug2.=sprintf("Transponder %s: New best time: %s (better than %s)\n",$transponder,$round['lapTime'],$this->best_rounds[$transponder]);
-				$this->best_rounds[$transponder]=$round['lapTime'];
+				$rounds[$key]['best-time']=$round['lap_time'];
+				$debug2.=sprintf("Transponder %s: New best time: %s (better than %s)\n",$transponder,$round['lap_time'],$this->best_rounds[$transponder]);
+				$this->best_rounds[$transponder]=$round['lap_time'];
 			}
 			else
 			{
 				$rounds[$key]['best-time']=$this->best_rounds[$transponder];
-				$debug2.=sprintf("Tranponder %s: Current time: %s (Best is %s)\n",$transponder,$round['lapTime'],$this->best_rounds[$transponder]);
+				$debug2.=sprintf("Tranponder %s: Current time: %s (Best is %s)\n",$transponder,$round['lap_time'],$this->best_rounds[$transponder]);
 			}
 
-			$this->previous_round[$round['transponder']]=$round['lapTime'];
+			$this->previous_round[$round['transponder']]=$round['lap_time'];
 		}
 		if($save_debug_info && isset($debug) && isset($debug2)) {
             file_put_contents('debug.txt', $debug);
--- tests/lap_timesTest.php
+++ tests/lap_timesTest.php
@@ -1,10 +1,22 @@
 <?php
 
+use datagutten\amb\laps\lap_timing;
 use PHPUnit\Framework\TestCase;
 use datagutten\amb\laps\lap_timing as laptimes;
 
 class lap_timesTest extends TestCase
 {
+    /**
+     * @var laptimes
+     */
+    protected $laps;
+
+    public function setUp(): void
+    {
+        $config = require __DIR__.'/test_config_db.php';
+        $this->laps = new lap_timing('20f93', $config);
+    }
+
     function testQueryToday()
     {
         $q = laptimes::query_today(1588188349);
@@ -15,59 +27,78 @@ class lap_timesTest extends TestCase
         $this->assertEquals(sprintf('rtc_time>=%d AND rtc_time<=%d', $time_today_start, $time_today_end), $q);
     }
 
-    function testInvalidLimit()
+    public function testInvalidLimit()
     {
-        set_include_path(__DIR__);
-        chdir('..');
-        $times = new laptimes('20f93');
         $this->expectException(UnexpectedValueException::class);
-        $this->expectExceptionMessage('Limit is not numeric');
-        $times->rounds('asdf');
+        $this->expectExceptionMessage('Lap count limit is not numeric');
+        $this->laps->laps('a');
+    }
+
+    public function testInvalidLapTimeLimit()
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->laps->laps(2, 'a');
+    }
+    public function testInvalidStartTime()
+    {
+        $this->expectException(UnexpectedValueException::class);
+        $this->laps->laps(2, 60, 'a');
+    }
+
+    public function testRounds()
+    {
+        $lap_times = $this->laps->laps(5);
+        $round =[
+            'lap_time' => 37.643,
+            'transponder' => 7824878,
+            'start_time' => 1588188349,
+            'end_time' => 1588188387,
+            'start_number' => 470764,
+            'end_number' => 470765];
+
+        $this->assertEquals($round, $lap_times[0]);
+    }
+
+    public function testConvertTime()
+    {
+        $time = lap_timing::convert_time(1588188349800000);
+        $this->assertEquals(1588188349, $time);
     }
 
     function testStats()
     {
-        set_include_path(__DIR__);
-        chdir('..');
-        $times = new laptimes('20f93');
-        //$q = 'SELECT * FROM passings_20f93 WHERE transponder=7824878';
-        $rounds = $times->rounds(10);
-        $stats = $times->stats($rounds);
+        $rounds = $this->laps->laps(5);
+        $stats = $this->laps->stats($rounds);
         $this->assertEquals('slower', $stats[0]['class']);
-        $this->assertEquals('19:25:49 29.04.2020', $stats[0]['datetime']);
+        $this->assertEquals(strtotime('19:25:49 29.04.2020'), $stats[0]['start_time']);
         $this->assertEquals('best-time', $stats[1]['class']);
     }
 
-    function testLimitRounds()
+    public function testLimitRounds()
     {
-        set_include_path(__DIR__);
-        chdir('..');
-        $times = new laptimes('20f93');
-        $rounds = $times->rounds(10);
+        $this->assertCount(5, $this->laps->laps(5));
+        $this->assertCount(10, $this->laps->laps(10));
+        $this->assertCount(16, $this->laps->laps(16));
+        $this->assertCount(50, $this->laps->laps(50));
+    }
+
+    function testLimitRounds2()
+    {
+        $rounds = $this->laps->laps(10);
         $this->assertLessThanOrEqual(10, count($rounds));
         $round =[
-            'round_time' => 37643000,
-            'lapTime' => 37.643,
+            'lap_time' => 37.643,
             'transponder' => 7824878,
             'start_time' => 1588188349,
-            'first_passing' => 470764,
-            'last_passing' => 470765,
-            'avatar' => null,
-            'nickname' => 'PetterS',
-            'datetime' => '19:25:49 29.04.2020',
-            'best-time' => null,
-            'class' => null,
-            'transponder_id' => '7824878',
-            'transponder_name'=> 'PetterS'];
+            'end_time' => 1588188387,
+            'start_number' => 470764,
+            'end_number' => 470765];
         $this->assertEquals($round, $rounds[0]);
     }
 
     function testUniqueTransponders()
     {
-        set_include_path(__DIR__);
-        chdir('..');
-        $times = new laptimes('20f93');
-        $transponders = $times->unique_transponders(1588188349);
+        $transponders = $this->laps->unique_transponders(1588188349);
         $this->assertEquals([8432586, 2546398, 2484233, 5589166, 7824878], $transponders);
     }
 }
