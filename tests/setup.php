a7f69e6a203362f02bf910bfce0e5010e07a6001
datagutten <datagutten@datagutten.net>
2021-07-10 11:10:07 +0200

    Wait for MySQL to start

--- tests/setup.php
+++ tests/setup.php
@@ -6,6 +6,7 @@ use datagutten\tools\PDOConnectHelper;
 require __DIR__ . '/../vendor/autoload.php';
 
 copy(files::path_join(__DIR__, 'test_config_db.php'), files::path_join(__DIR__, 'config_db.php'));
+sleep(5);
 
 $db = PDOConnectHelper::connect_db_config(['db_host' => '127.0.0.1', 'db_user' => 'root', 'db_password' => 'root', 'db_name' => 'passings_test', 'db_port' => 3800]);
 
