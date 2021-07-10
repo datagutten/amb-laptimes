9e34258f77835233efd9a35cb1043a34b0719fe8
datagutten <datagutten@datagutten.net>
2021-07-10 20:38:51 +0200

    Always recreate test database

--- tests/passing_dbTest.php
+++ tests/passing_dbTest.php
@@ -15,68 +15,60 @@ class passing_dbTest extends TestCase
     {
         $config = require __DIR__.'/test_config.php';
         $this->passings = new passing_db('test_decoder', $config['db']);
+
+        $this->passings->db->query('DROP TABLE IF EXISTS transponders');
+        $this->passings->db->query('DROP TABLE IF EXISTS passings_test_decoder');
+
+        $this->passings->create_table('test_decoder');
+        $this->passings->db->query(file_get_contents(__DIR__.'/../src/transponders.sql'));
     }
 
     public function testCreate_table()
     {
-        $passings = $this->passings;
-        $passings->create_table('test_decoder');
-
         try {
-            $st = $passings->db->query('SELECT * FROM passings_test_decoder');
+            $st = $this->passings->db->query('SELECT * FROM passings_test_decoder');
             $this->assertEquals(0, $st->rowCount());
         }
         catch (PDOException $e)
         {
             $this->fail($e->getMessage());
         }
-
-        //$passings->insert()
-        $passings->db->query('DROP TABLE passings_test_decoder');
     }
 
     public function testInsert()
     {
         $passings = $this->passings;
-        try {
-            $passings->create_table('test_decoder');
-            $passings->init();
+        $passings->init();
 
-            $passing = [
-                'version' => 2,
-                'length' => 51,
-                'crc' => 'c79e',
-                'flags_header' => 0000,
-                'type' => 1,
-                'record_hex' => '8e0233009ec700000100010448bc010003041d582c000408e805bfc4a41b050005026a0006023200080200008104930f02008f',
-                'PASSING_NUMBER' => 113736,
-                'TRANSPONDER' => 2906141,
-                'RTC_TIME' => 1437769372993000,
-                'STRENGTH' => 106,
-                'HITS' => 50,
-                'FLAGS' => 0,
-                'DECODER_ID' => 135059
-            ];
-            $passings->insert($passing);
-            $st = $passings->db->query('SELECT * FROM passings_test_decoder WHERE rtc_time=1437769372993000');
-            $this->assertEquals(1, $st->rowCount());
-        }
-        finally {
-            $passings->db->query('DROP TABLE passings_test_decoder');
-        }
+        $passing = [
+            'version' => 2,
+            'length' => 51,
+            'crc' => 'c79e',
+            'flags_header' => 0000,
+            'type' => 1,
+            'record_hex' => '8e0233009ec700000100010448bc010003041d582c000408e805bfc4a41b050005026a0006023200080200008104930f02008f',
+            'PASSING_NUMBER' => 113736,
+            'TRANSPONDER' => 2906141,
+            'RTC_TIME' => 1437769372993000,
+            'STRENGTH' => 106,
+            'HITS' => 50,
+            'FLAGS' => 0,
+            'DECODER_ID' => 135059
+        ];
+        $passings->insert($passing);
+        $st = $passings->db->query('SELECT * FROM passings_test_decoder WHERE rtc_time=1437769372993000');
+        $this->assertEquals(1, $st->rowCount());
     }
 
     public function testTransponders()
     {
-        $passings = $this->passings;
-        $transponders = $passings->transponders(true);
+        $transponders = $this->passings->transponders(true);
         $this->assertTrue(is_array($transponders));
     }
 
     public function testTransponders2()
     {
-        $passings = $this->passings;
-        $transponders = $passings->transponders();
+        $transponders = $this->passings->transponders();
         $this->assertInstanceOf('PDOStatement', $transponders);
     }
 }
