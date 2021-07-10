a1d7b187c97c53c4fe9da2147c2b3fc918b67404
datagutten <datagutten@datagutten.net>
2021-07-10 10:49:40 +0200

    Use 127.0.0.1 instead of localhost

--- tests/setup.php
+++ tests/setup.php
@@ -7,7 +7,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 copy(files::path_join(__DIR__, 'test_config_db.php'), files::path_join(__DIR__, 'config_db.php'));
 
-$db = PDOConnectHelper::connect_db_config(['db_host' => 'localhost', 'db_user' => 'root', 'db_password' => 'root', 'db_name' => 'passings_test', 'db_port' => 3800]);
+$db = PDOConnectHelper::connect_db_config(['db_host' => '127.0.0.1', 'db_user' => 'root', 'db_password' => 'root', 'db_name' => 'passings_test', 'db_port' => 3800]);
 
 $db->query(file_get_contents(__DIR__ . '/test_data/passings.sql'));
 $db->query(file_get_contents(__DIR__ . '/test_data/transponders.sql'));
