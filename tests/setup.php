86405ab3ec9f47e70af15de3025805b63530c97f
datagutten <datagutten@datagutten.net>
2021-07-10 11:50:58 +0200

    Use built-in MySQL

--- tests/setup.php
+++ tests/setup.php
@@ -8,7 +8,7 @@ require __DIR__ . '/../vendor/autoload.php';
 copy(files::path_join(__DIR__, 'test_config_db.php'), files::path_join(__DIR__, 'config_db.php'));
 sleep(5);
 
-$db = PDOConnectHelper::connect_db_config(['db_host' => '127.0.0.1', 'db_user' => 'root', 'db_password' => 'root', 'db_name' => 'passings_test', 'db_port' => 3800]);
+$db = PDOConnectHelper::connect_db_config(['db_host' => '127.0.0.1', 'db_user' => 'root', 'db_password' => 'root', 'db_name' => 'passings_test']);
 
 $db->query(file_get_contents(__DIR__ . '/test_data/passings.sql'));
 $db->query(file_get_contents(__DIR__ . '/test_data/transponders.sql'));
