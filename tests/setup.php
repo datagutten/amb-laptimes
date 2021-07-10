f05b6cd0df00234bca5e499c18fabf9ea82116fb
datagutten <datagutten@datagutten.net>
2021-07-10 12:11:05 +0200

    Use environment variables

--- tests/setup.php
+++ tests/setup.php
@@ -6,9 +6,10 @@ use datagutten\tools\PDOConnectHelper;
 require __DIR__ . '/../vendor/autoload.php';
 
 copy(files::path_join(__DIR__, 'test_config_db.php'), files::path_join(__DIR__, 'config_db.php'));
-sleep(5);
+$config = require 'test_config_db.php';
 
-$db = PDOConnectHelper::connect_db_config(['db_host' => '127.0.0.1', 'db_user' => 'root', 'db_password' => 'root', 'db_name' => 'passings_test']);
+
+$db = PDOConnectHelper::connect_db_config($config);
 
 $db->query(file_get_contents(__DIR__ . '/test_data/passings.sql'));
 $db->query(file_get_contents(__DIR__ . '/test_data/transponders.sql'));
