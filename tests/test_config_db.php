220ef4d4725b639509e0efe8c19f186c6b6b615a
datagutten <datagutten@datagutten.net>
2021-07-10 10:58:53 +0200

    Update test database config

--- tests/test_config_db.php
+++ tests/test_config_db.php
@@ -1,7 +1,8 @@
 <?Php
-$config['db_host']="localhost";
-$config['db_user']="php_test";
-$config['db_password']="password";
+$config['db_host']="127.0.0.1";
+$config['db_user']="root";
+$config['db_password']="root";
 $config['db_name']='passings_test';
 $config['db_type']='mysql';
+$config['db_port']=3800;
 return $config;
